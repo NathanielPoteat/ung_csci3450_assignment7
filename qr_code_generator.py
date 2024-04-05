3266a378de0f8cd8e4980fd963991884d73f660d
RedDragon-11 <mgaddis97@gmail.com>
2024-04-05 16:36:32 -0400

    Finished QR Codes and Model
    You're going to have to Extract the Zip file to use the model. Chance the output directory in the qr_code_generator file to whatever you want to use.

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,24 +1,37 @@
-# This code is courtesy of ChatGPT
-import csv
+import os
 import qrcode
+import pandas
 
 # Loop through each line in the .csv file and convert the URL into a QR Code
-def generate_qr_codes(csv_file, output_directory):
-    with open(csv_file, newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            url = row[0]
-            category = row[1]
-            qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-            qr.add_data(url)
-            qr.make(fit=True)
-            img = qr.make_image(fill_color="black", back_color="white")
+def generate_qr_codes(csv_file, output_directory_bad, output_directory_good):
+    count = 0
+    data = pandas.read_csv(csv_file, names = ['url','label'])
 
-            if category == 'good':
-                img.save(f"{output_directory}/good/{url.replace('/','_')}.png")
-            elif category == 'bad':
-                img.save(f"{output_directory}/bad/{url.replace('/','_')}.png")
+    for i, row in data.iterrows():
+        url = data['url']
+        label = data['label']
+        qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
+        qr.add_data(url[i])
+        qr.make(fit=True)
+        img = qr.make_image(fill_color="black", back_color="white")
+        count += 1
+        if label[i] == 'good':
+            if not os.path.exists(f"{output_directory_good}"):
+                os.makedirs(f"{output_directory_good}")
+            filename = (f"qrgood{count}.png")
+            image_path = os.path.join((f"{output_directory_good}"), filename)
+            img.save(image_path)
+            print(f"QR, {filename} saved to: {image_path}")  
 
-csv_file = "./phishing_site_urls.csv"
-output_directory = "./phishing_qr_codes"
-generate_qr_codes(csv_file, output_directory)
+        elif label[i] == 'bad':
+            if not os.path.exists(f"{output_directory_bad}"):
+               os.makedirs(f"{output_directory_bad}")
+            filename = (f"qrbad{count}.png")
+            image_path = os.path.join((f"{output_directory_bad}"), filename)
+            img.save(image_path)
+            print(f"QR, {filename} saved to: {image_path}")
+            
+csv_file = r"C:\Users\Matthew\Desktop\PersonalProjects\ung_csci3450_assignment7\phishing_site_urls.csv"
+output_directory_bad = r"C:\Users\Matthew\Desktop\PersonalProjects\ung_csci3450_assignment7\phishing_qr_codes\bad"
+output_directory_good = r"C:\Users\Matthew\Desktop\PersonalProjects\ung_csci3450_assignment7\phishing_qr_codes\good"
+generate_qr_codes(csv_file, output_directory_bad, output_directory_good)
